1d969163a4f15ca26f101df8297a7fda7966518c
Himani <h32@myseneca.ca>
2022-10-07 11:31:04 -0400

    initial commit

--- server.js
+++ server.js
@@ -5,71 +5,73 @@
 * 
 *  Name: ____HIMANI__________________ Student ID: ___155567217___________ Date: __09/30/2022______________
 *
-*  Online (Cyclic) Link: ____https://drab-gold-dibbler-tam.cyclic.app/____________________________________________________
+*  Online (Cyclic) Link: ________________________________________________________
 *
 ********************************************************************************/
 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
-var path = require('path');
-var blogservice = require(__dirname + '/blog-service');
-app.use(express.static('public')); 
+var path = require("path");
+var blogservice = require(__dirname + "/blog-service");
+app.use(express.static("public"));
 
-onHttpStart = () => 
-{
-    console.log('Express http server listening on ' + HTTP_PORT);
-}
+onHttpStart = () => {
+  console.log("Express http server listening on " + HTTP_PORT);
+};
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-{
-    res.redirect('/about')
+app.get("/", (req, res) => {
+  res.redirect("/about");
 });
 
-app.get('/about', (req, res) => 
-{
-    res.sendFile(path.join(__dirname + "/views/about.html"));
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
-app.get("/blog", (req, res) => 
-{
-    blogservice.getPublishedPosts().then((data) =>
-    {
-        res.json({data});
-    }).catch((err) => {
-        res.json({message: err});
+app.get("/blog", (req, res) => {
+  blogservice
+    .getPublishedPosts()
+    .then((data) => {
+      res.json({ data });
     })
+    .catch((err) => {
+      res.json({ message: err });
+    });
 });
 
-app.get("/data/posts.json", (req, res) => 
-{
-    blogservice.getAllPosts().then((data) =>
-    {
-        res.json({data});
-    }).catch((err) => {
-        res.json({message: err});
+app.get("/data/posts.json", (req, res) => {
+  blogservice
+    .getAllPosts()
+    .then((data) => {
+      res.json({ data });
     })
+    .catch((err) => {
+      res.json({ message: err });
+    });
 });
 
-app.get("/data/categories.json", (req, res) => 
-{
-    blogservice.getCategories().then((data) =>
-    {
-        res.json({data});
-    }).catch((err) => {
-        res.json({message: err});
+app.get("/data/categories.json", (req, res) => {
+  blogservice
+    .getCategories()
+    .then((data) => {
+      res.json({ data });
     })
+    .catch((err) => {
+      res.json({ message: err });
+    });
 });
 
-app.get('*', function(req, res){
-    res.status(404).send("Page Not Found!");
-  });
+app.get("*", function (req, res) {
+  res.status(404).send("Page Not Found!");
+});
 
-blogservice.initialize().then(() => 
-{
+blogservice
+  .initialize()
+  .then(() => {
     app.listen(HTTP_PORT, onHttpStart());
-}).catch (() => {
+  })
+  .catch(() => {
     console.log("ERROR : From starting the server");
-});
+  });
